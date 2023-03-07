d93ef9e2d6c1d26c2bf27bcfe63b30d1adaaf473
Igor-Ranielli <igorranielli@hotmail.com>
2023-03-07 20:33:31 -0300

    Version 1.0

---
+++ projeto_buscapreco/app_buscapreco/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Produto(models.Model):
+    id_produto = models.AutoField(primary_key=True)
+    nome = models.TextField(max_length=255)
+    preco = models.DecimalField(decimal_places=2, max_digits=12)
+    site = models.TextField(max_length=255)
+    data_cotacao = models.DateField()
+    link_imagem = models.TextField(max_length=500, default='https://via.placeholder.com/150')
